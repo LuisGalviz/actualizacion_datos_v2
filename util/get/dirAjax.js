eb76343fc4389a06541a559d8a0fb074b8354f27
LuisGalviz <lfgalviz@hotmail.com>
2023-02-19 03:41:51 -0500

    async form dir

--- util/get/dirAjax.js
+++ util/get/dirAjax.js
@@ -1,84 +1,74 @@
-function getDir(typeInfo, type, idIndex, usr, idform) {
+async function getDir(typeInfo, type, idIndex, usr, idform) {
   const ciudad = $("#ciudad" + idform);
-  let pidm = getPidm(usr);
-  pidm.done(function (data) {
-    $.ajax({
+  try {
+    const data = await getPidm(usr);
+    const element = await $.ajax({
       type: "GET",
       url: `${pdimEndpoint}` + data["pidm"] + "/" + typeInfo + "/tipo/" + type,
-      success: function (element) {
-        if (element.length > 0) {
-          //  console.log(element);
-          let html = `${element[0]["line1"]}<br>${element[0]["line2"]}<br>${element[0]["line3"]}<br>`;
-          $(idIndex).html(html);
+    });
+    if (element.length > 0) {
+      let html = `${element[0]["line1"]}<br>${element[0]["line2"]}<br>${element[0]["line3"]}<br>`;
+      $(idIndex).html(html);
 
-          if (element[0]["nation"] == "COL") {
-            buscarDpto().done(function (datos) {
-              datos.forEach((element2) => {
-                if (
-                  element2["codigo"] == element[0]["state"] &&
-                  element[0]["state"] != "0"
-                ) {
-                  $.getJSON(
-                    `${dptoEndpoint}${element[0]["state"]}/ciudades`,
-                    function (data) {
-                      $.each(data, function (key, value) {
-                        ciudad.append(
-                          $("<option>", {
-                            value: value.codigo,
-                            text: value.descripcion,
-                          })
-                        );
-                      });
-                    }
-                  );
-
-                  $("#departamento" + idform).val(element2["codigo"]);
-                  //  console.log(element2["codigo"]);
-                  $(idIndex).append(element2["descripcion"] + "<br>");
-                  buscarCiudad(element2["codigo"]).done(function (datos) {
-                    datos.forEach((element3) => {
-                      if (element3["codigo"] == element[0]["city"]) {
-                        ciudad.val(element3["codigo"]);
-                        $(idIndex).append(element3["descripcion"] + "<br>");
-                      }
-                    });
-                  });
-                }
-              });
+      if (element[0]["nation"] == "COL") {
+        const datos = await buscarDpto();
+        datos.forEach(async (element2) => {
+          if (
+            element2["codigo"] == element[0]["state"] &&
+            element[0]["state"] != "0"
+          ) {
+            const data = await $.getJSON(
+              `${dptoEndpoint}${element[0]["state"]}/ciudades`
+            );
+            await $.each(data, function (key, value) {
+              console.log('holi');
+              ciudad.append(
+                $("<option>", {
+                  value: value.codigo,
+                  text: value.descripcion,
+                })
+              );
             });
-          } else {
-            if (idform == "P") {
-              departamentoP.prop("disabled", true).val("0");
-              ciudadP.prop("disabled", true).val("0");
-            } else if (idform == "T") {
-              departamentoT.prop("disabled", true).val("0");
-              ciudadT.prop("disabled", true).val("0");
-            }
-          }
 
-          buscarPais().done(function (datos) {
-            datos.forEach((element2) => {
-              if (element2["codigo"] == element[0]["nation"]) {
-                $("#pais" + idform).val(element2["codigo"]);
-                // console.log(element2["descripcion"]);
-                $(idIndex).append(element2["descripcion"] + "<br>");
+            $("#departamento" + idform).val(element2["codigo"]);
+            $(idIndex).append(element2["descripcion"] + "<br>");
+            const datosCiudad = await buscarCiudad(element2["codigo"]);
+            datosCiudad.forEach((element3) => {
+              if (element3["codigo"] == element[0]["city"]) {
+                console.log('holi222');
+                ciudad.val(element3["codigo"]);
+                $(idIndex).append(element3["descripcion"] + "<br>");
               }
             });
-          });
-
-          $("#direccion" + idform).val(element[0]["line1"]);
-          $("#complemento" + idform).val(element[0]["line2"]);
-          $("#barrio" + idform).val(element[0]["line3"]);
-          ciudad.val(element[0]["city"]);
-          $("#seq" + idform).val(element[0]["seq"]);
+          }
+        });
+      } else {
+        if (idform == "P") {
+          departamentoP.prop("disabled", true).val("0");
+          ciudadP.prop("disabled", true).val("0");
+        } else if (idform == "T") {
+          departamentoT.prop("disabled", true).val("0");
+          ciudadT.prop("disabled", true).val("0");
         }
-      },
-      error: function (error) {
-        return error;
-        // Acción a realizar en caso de error en la petición
-      },
-    });
-  });
+      }
+
+      const datosPais = await buscarPais();
+      datosPais.forEach((element2) => {
+        if (element2["codigo"] == element[0]["nation"]) {
+          $("#pais" + idform).val(element2["codigo"]);
+          $(idIndex).append(element2["descripcion"] + "<br>");
+        }
+      });
+
+      $("#direccion" + idform).val(element[0]["line1"]);
+      $("#complemento" + idform).val(element[0]["line2"]);
+      $("#barrio" + idform).val(element[0]["line3"]);
+      ciudad.val(element[0]["city"]);
+      $("#seq" + idform).val(element[0]["seq"]);
+    }
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 getDir("direccion", "DP", "#dirPermanenteAjax", userUn, "P");
